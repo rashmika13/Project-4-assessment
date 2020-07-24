a24e97c31c0d4407356209cc510d282a487205ec
R <50966008+rashmika13@users.noreply.github.com>
2020-07-24 18:54:16 -0400

    Updates

---
+++ precache-manifest.6c7ffd7ade7b96f4d02e2c0038dca09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f559a4c5c3d4e1d209bca5d012d4529",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "3eee1359cf4060c7623a",
+    "url": "/project-4-assessment/static/css/main.d9fbd6fb.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eee1359cf4060c7623a",
+    "url": "/project-4-assessment/static/js/main.0c4f9465.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
